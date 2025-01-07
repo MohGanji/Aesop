6f83fb2f1cc11017c2076889192eb5d1687f9deb
Mo Ganji <mo@ganji.me>
2025-01-06 16:28:42 -0800

    cache requests with same data so we do fewer requests when developing

--- server/app.py
+++ server/app.py
@@ -3,14 +3,34 @@ from flask import Flask, request, Response, stream_with_context
 from flask_cors import CORS
 import requests
 from dotenv import load_dotenv
+from functools import lru_cache
 
-load_dotenv()  # Load environment variables from .env file
+load_dotenv() 
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 ELEVENLABS_API_KEY = os.environ.get('ELEVENLABS_API_KEY')
 ELEVENLABS_API_URL = "https://api.elevenlabs.io/v1/text-to-speech/{voice_id}/stream"
+
+@lru_cache(maxsize=1024)
+def elevenlabs_tts(voice_id, text):
+    headers = {
+        "Accept": "audio/mpeg",
+        "xi-api-key": ELEVENLABS_API_KEY,
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "text": text,
+        "model_id": "eleven_turbo_v2_5",
+        "voice_settings": {
+            "stability": 0.5,
+            "similarity_boost": 0.75
+        }
+    }
+    response = requests.post(ELEVENLABS_API_URL.format(voice_id=voice_id), json=payload, headers=headers, stream=True)
+    response.raise_for_status()
+    return b"".join(response.iter_content(chunk_size=8192))
 
 @app.route('/tts', methods=['POST'])
 def text_to_speech():
@@ -24,29 +44,13 @@ def text_to_speech():
     if not text:
         return {"error": "No text provided"}, 400
 
-    headers = {
-        "Accept": "audio/mpeg",
-        "xi-api-key": ELEVENLABS_API_KEY,
-        "Content-Type": "application/json"
-    }
-
-    payload = {
-        "text": text,
-        "model_id": "eleven_turbo_v2_5",
-        "voice_settings": {
-            "stability": 0.5,
-            "similarity_boost": 0.75
-        }
-    }
-
     def generate():
-        with requests.post(ELEVENLABS_API_URL.format(voice_id=voice_id), json=payload, headers=headers, stream=True) as r:
-            r.raise_for_status()
-            for chunk in r.iter_content(chunk_size=8192):
-                yield chunk
+        cached_audio = elevenlabs_tts(voice_id, text)
+        print(elevenlabs_tts.cache_info())
+        for i in range(0, len(cached_audio), 8192):
+            yield cached_audio[i:i + 8192]
 
     return Response(stream_with_context(generate()), content_type="audio/mpeg")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5005', debug=True)
-
